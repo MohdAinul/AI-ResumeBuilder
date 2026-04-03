0b4605202dee168e616b24897ed95c3885a6c811
MohdAinul <mohdainul01@gmail.com>
2026-04-03 21:19:15 +0530

    add features section,Hero section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/home/Hero";
 import Banner from "../components/home/Banner";
+import Features from "../components/home/Features";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Hero />
+      <Features />
     </div>
   );
 };
