ac94e1d1d6aa96f440819c966da151d046ff9631
MohdAinul <mohdainul01@gmail.com>
2026-04-09 11:16:32 +0530

    create resume controller api

---
+++ server/controllers/resumeController.js
@@ -0,0 +1,19 @@
+import Resume from "../models/Resume.js";
+
+//controller for creating a new resume
+// POST: /api/resumes/create
+export const createResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const { title } = req.body;
+
+    const newResume = await Resume.create({ userId, title });
+
+    return res
+      .status(201)
+      .json({ message: "Resume create successfully", resume: newResume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
